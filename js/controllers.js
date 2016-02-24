33f8c534837d2dcd61e0d9ead458fef6e2a94b0e
Niall O'Reilly <nialloreilly123@gmail.com>
2016-02-24 12:47:55 +0000

    wed lunchtime

--- js/controllers.js
+++ js/controllers.js
@@ -14,166 +14,223 @@ angular.module('controllersContainer', [])
 })
 .controller('MainScreenController', function($scope, $rootScope, $http, $timeout){
 
+	var allSongs = [];//all 30 songs that will be loaded from spotify
+	var currentSongs = {}; //3 songs to be used per round
+	var currentRound = 1;
+	var playingSong;//song object of the currently playing song
+	var pickedSongNum;///which of the 3 songs of the round will be played
+
+	$scope.makeChoice = makeChoice;
+	$scope.begin =  startRound;
+	$scope.displayChoices = false;
 
 
-function initialise(){
-
-	function randomNumberBetween(min,max)
-	{
+	function randomNumberBetween(min,max){
 	    return Math.floor(Math.random()*(max-min+1)+min);
 	}
 
-	///Set spotify's list offset based on the difficulty setting
-	///The higher the difficulty, the less popular songs will be shown
-	function getOffset(diff){
-
-		var offset = 0;
-		var offset_min;
-		var offset_max;
-
-		if(diff === 'easy'){
-			offset_min = 0; ///only possible to get the 100 most popular songs
-			offset_max = 70;
-		}
-		else if(diff === 'medium'){
-			offset_min = 600;
-			offset_max = 1200;
-		}
-		else{
-			offset_min = 4000;
-			offset_max = 7000;
-		}
-
-		
-		offset = randomNumberBetween(offset_min, offset_max);
-		return String(offset);
-	}
-
-	
-
-	function getSpotifyUrl(){
-		return 'https://api.spotify.com/v1/search?q=year%3A1900-2040&type=track&market=IE&limit=30&offset='+getOffset($rootScope.difficulty);
-	}
-
-	function fetchTrackData(){
-		$http.get(getSpotifyUrl()).then(function(response){
-			
-			songs =  response.data.tracks.items;
-			setupEnvironment(songs);
-			
-			console.log(songs);
-
-		})
-	}
-
-	var currentRound = 1;
-
-	function getSongs(songs, round){///get 3 songs based on what round it is
-		var startingPoint = (round*3)-3;
-		song1 = songs[startingPoint];
-		song2 = songs[startingPoint+1];
-		song3 = songs[startingPoint+2];
-		return [song1,song2,song3];
-	}
-
-	function setupEnvironment(songs){
-		var currentSongs = getSongs(songs, currentRound);
-		$scope.currentSongs = currentSongs;
-		var playingSong = pickASong(currentSongs);
-
-		loadMusic(playingSong);		
-		
-	}
-
-	function loadMusic(song){
-		angular.element('#music-player')[0].src = song.preview_url;
-	}
-
-	$rootScope.startMusic = function (){
+	$rootScope.startMusic = function (){ ///start audio /// needs to be accessible from any controller
 		document.getElementById('music-player').play();
 	}
 
-	$rootScope.stopMusic = function (){
+	$rootScope.stopMusic = function (){///stop audio /// needs to be accessible from any controller
 		document.getElementById('music-player').pause();
 	}
-	
 
-	function pickASong(currentSongs){
-		pickedSongNum = randomNumberBetween(0,2);
-		return currentSongs[ pickedSongNum ];
+	function resetTimer(){
+		$scope.currentTimer = 1000;
+		$scope.newTimer = 1000;
+	}	
+
+	function getTimer(){
+		var timer = $('#timer').text();
+		return Number(timer);
 	}
-fetchTrackData();
+
+	function startTimer(){
+		$scope.newTimer = 0;
+	}
+
+	function stopTimer(){
+		$scope.currentTimer = getTimer();
+		$scope.newTimer = getTimer();
+	}
 
 
+	(function initialiseGame(){
+		$scope.currentScore = 0;
 
+		///Set spotify's list offset based on the difficulty setting
+		///The higher the difficulty, the less popular songs will be shown
+		function getOffset(diff){
 
-};
-initialise();
-resetTimer();
+			var offset = 0;
+			var offset_min;
+			var offset_max;
 
-var pickedSongNum;
+			if(diff === 'easy'){
+				offset_min = 0; ///only possible to get the 100 most popular songs
+				offset_max = 70;
+			}
+			else if(diff === 'medium'){
+				offset_min = 600;
+				offset_max = 1200;
+			}
+			else{
+				offset_min = 4000;
+				offset_max = 7000;
+			}
 
-$scope.makeChoice = function(num){
-	checkAnswer(num);
+			
+			offset = randomNumberBetween(offset_min, offset_max);
+			return String(offset);
+		}
 
+		function getSpotifyUrl(){
+			return 'https://api.spotify.com/v1/search?q=year%3A1900-2040&type=track&market=IE&limit=30&offset='+getOffset($rootScope.difficulty);
+		}
 
-}
+		function fetchTrackData(){
+			$http.get(getSpotifyUrl()).then(function(response){
+				
+				allSongs =  response.data.tracks.items;
+				setupRound(1);			
+				console.log('songs fetched');
+			})
+		}
+		fetchTrackData();
+	})();
 
+	function setupRound(round){
 
-function setupRound(round){};
+		function getCurrentSongs(round){///get 3 songs based on what round it is
+			var startingPoint = (round*3)-3;
+			song1 = allSongs[startingPoint];
+			song2 = allSongs[startingPoint+1];
+			song3 = allSongs[startingPoint+2];
+			return [song1,song2,song3];
+		}
 
+		function pickASong(currentSongs){///pick a random song out of the 3 songs per round
+			pickedSongNum = randomNumberBetween(0,2);
+			return currentSongs[ pickedSongNum ];
+		}
 
-function resetTimer(){
-	$scope.currentTimer = 1000;
-	$scope.newTimer = 1000;
-}
+		function loadMusic(song){///load the passed song into the player
+			angular.element('#music-player')[0].src = song.preview_url;
+		}
 
+		function loadSleeve(){
+			///load new sleeve
+		}
 
-function getTimer(){
-	var timer = $('#timer').text();
-	return Number(timer);
-}
+		function loadRecord(){
+			///load record to turntable
+		}
 
-function startTimer(){
-	$scope.newTimer = 0;
-}
+		resetTimer();
+		currentSongs = getCurrentSongs(round);
+		$scope.currentSongs = currentSongs;
+		playingSong = pickASong(currentSongs);
+		loadMusic(playingSong);
+		console.log('round setup');
+	}
 
-function stopTimer(){
-	$scope.currentTimer = getTimer();
-	$scope.newTimer = getTimer();
-}
+	function startRound(){
 
-function correctAnswer(){
+		function needleOn(){
+			////rotate the needle element
+		}
 
-}
+		function startTurntable(){
+			needleOn();
+			///after 1s start music
+			///setTimeout
+			$rootScope.startMusic();
+			startTimer();	
+		}
+		$scope.displayChoices = true;
 
-var checkAnswer = function(num){
-	$rootScope.stopMusic();
-	stopTimer();
-	console.log('checkAnswer');
-	if(num == pickedSongNum){
+		///startDisplay()
+		startTurntable();
+	}	
+
+	function stopRound(){
+		function needleOff(){
+			//rotate the needle element
+		}
+
+		function stopTurntable(){
+			needleOff();
+			$rootScope.stopMusic;
+		}
+
+		stopTimer();	
+		///stopDisplay()
+		stopTurntable()
+	}
+
+	function makeChoice(num){
 		
-		increaseScore(getTimer());
-		return true;
-	}
-	else{
-		return false;
-	}
-}
+		function checkAnswer(num){
+			if(num == pickedSongNum){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
 
-$scope.begin =  function(){
-	$rootScope.startMusic();
-	startTimer();
-}
+		function revealAnswer(){
+			///spin sleeve
+			///make button green
+		}
 
-$scope.currentScore = 0;
-function increaseScore(score){
+		function correctAnswer(){
+			function increaseScore(score){
+				$scope.newScore = $scope.currentScore + score;
+				$timeout(function(){
+					$scope.currentScore = $scope.newScore;
+				}, 1000);
 
-	$scope.newScore = $scope.currentScore + score;
-	$timeout(function(){
-		$scope.currentScore = $scope.newScore;
-	}, 1000);
-}
+				///play flicking sfx
+			}
+
+			increaseScore(getTimer());
+			//play correct sfx
+		}
+
+		function incorrectAnswer(){
+			///play buzzer sfx
+		}
+
+
+		stopRound();
+		revealAnswer();
+		if(checkAnswer(num) == true){
+			correctAnswer();
+		}
+		else{
+			incorrectAnswer();
+		}
+		currentRound++;
+		setupRound(currentRound);
+	}//makeChoice()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //get songs
@@ -195,8 +252,6 @@ function increaseScore(score){
 		//flip sleeve
 		//record disappear
 		//sleeve disappear, new one in
-
-
 
 
 })
